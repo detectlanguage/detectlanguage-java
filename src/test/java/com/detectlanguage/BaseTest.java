0c87e6decbd3b5f324b7d175c8b5d821017191a1
Laurynas Butkus <laurynas.butkus@gmail.com>
2017-01-24 21:18:52 +0200

    Use test key from environment

--- src/test/java/com/detectlanguage/BaseTest.java
+++ src/test/java/com/detectlanguage/BaseTest.java
@@ -3,13 +3,11 @@ package com.detectlanguage;
 import org.junit.Before;
 
 public class BaseTest {
-    public static String TEST_API_KEY = "93dfb956a294140a4370a09584af2ef6";
-
     @Before
     public void setUp() {
         DetectLanguage.apiHost = System.getProperty("detectlanguage_api_host",
                 DetectLanguage.apiHost);
         DetectLanguage.apiKey = System.getProperty("detectlanguage_api_key",
-                TEST_API_KEY);
+                System.getenv().get("DETECTLANGUAGE_API_KEY"));
     }
 }
